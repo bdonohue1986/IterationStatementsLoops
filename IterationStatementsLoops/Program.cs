36b1a7440894d5212a846390d9d8bfddbb11abd2
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-10-10 08:55:13 -0400

    inital commit

---
+++ IterationStatementsLoops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IterationStatementsLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*{for (int i=0; i<5; i++)
+                Console.WriteLine($"I = {i}");
+        for (int a=0; a<5; a++)
+                Console.WriteLine($"A = {a}");
+        for (int b=0; b<5; b++)
+                Console.WriteLine($"B = {b}");
+        for (int c=0; c<5; c++)
+                Console.WriteLine($"C = {c}");
+        for (int d=0; d<5; d++)
+                Console.WriteLine($"D = {d}");
+        for (int e=0; e<5; e++)
+                   Console.WriteLine($"E = {e}");
+        for (int f=0; f<5; f++)
+                Console.WriteLine($" F = {f}");
+            for (int g = 0; g < 5; g++)
+                Console.WriteLine($"G = {g}");
+            for (int h = 0; h < 5; h++)
+                Console.WriteLine($"H = {h}");
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"I = {i}");
+            for (int j=0; j < 5; j++)
+                Console.WriteLine($"J = {j}");
+            for (int k=0; k < 5; k++)
+                Console.WriteLine($"K = {k}");
+            for (int l=0; l<5; l++)
+                Console.WriteLine($"L = {l}");
+            for (int m=0; m<5; m++)
+                Console.WriteLine($"M = {m}");
+            for (int n = 0; n < 5; n++)
+                Console.WriteLine($"N = {n}");
+            for (int o=0; o<5; o++)
+                Console.WriteLine($"O = {o}");
+            Console.WriteLine("Finished");
+        }*/
+        
+       
+           
+    }
+}
